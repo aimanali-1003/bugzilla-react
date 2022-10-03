6e7d3bae1f01057bb4f9473f21f23b19316e4d89
aimanali-1003 <aiman.ali@devsinc.com>
2022-10-03 21:54:58 +0500

    project show and listing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import logo from './logo.svg';
+// import React from 'react';
 import './App.css';
+// import { Route, Routes } from 'react-router-dom';
+import ProjectList from './components/ProjectList';
+import ProjectShow from './components/ProjectShow';
+import Bugshow from './components/Bugshow';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useRoutes,
+  BrowserRouter,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-     <index />
+    <div>
+     <BrowserRouter> 
+      <Routes>
+        <Route path='/' element={<ProjectList />}></Route>
+        <Route path='/ProjectShow' element={<ProjectShow />}></Route>
+
+        
+      </Routes>
+      {/* <Routes>
+        <Route path='/' component={ProjectList}></Route>
+      </Routes> */}
+     {/* <ProjectShow /> */}
+     </BrowserRouter>
     </div>
   );
 }
