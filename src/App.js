390a286bd4d07aab082689734e8481e54fad8a24
aimanali-1003 <aiman.ali@devsinc.com>
2022-10-06 14:28:53 +0500

    project listing styling and changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
-import ProjectList from './components/ProjectList';
-import ProjectShow from './components/ProjectShow';
-import BugList from './components/BugList';
+import ProjectList from './components/Project/ProjectList';
+import ProjectShow from './components/Project/ProjectShow';
+import BugList from './components/Bug/BugList';
 import React from "react";
+
 import {
   BrowserRouter as Router,
   Routes,
@@ -12,17 +13,23 @@ import {
 } from "react-router-dom";
 
 function App() {
+
   return (
     <div>
      <BrowserRouter> 
       <Routes>
         <Route path='/' element={<ProjectList />}></Route>
-        <Route path='/ProjectShow' element={<ProjectShow />}></Route>
-        <Route path='/BugList' element={<BugList />}></Route>      
+        
+        {/* <Route path='/ProjectShow' element={<ProjectShow />}></Route> */}
+        <Route path="/ProjectShow/:id" element={<ProjectShow />}></Route>
+        <Route path='/BugList' element={<BugList />}></Route>    
+        <Route path="/ProjectShow/:id/BugList" exact element={<BugList />}></Route>
+
       </Routes>
      </BrowserRouter>
     </div>
   );
+
 }
 
 export default App;
