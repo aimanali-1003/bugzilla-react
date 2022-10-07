691149179da6909b6587a9e3e22397c2e19cab9e
aimanali-1003 <aiman.ali@devsinc.com>
2022-10-07 13:58:49 +0500

    Bug API set up

--- src/components/Bug/BugList.js
+++ src/components/Bug/BugList.js
@@ -1,46 +1,56 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { BugList } from "../../api/bugAPI";
 
-export default function ShowBugListing(){
+export default function ShowBugListing() {
   const params = useParams();
-  const[bugs, setBug] = useState([]);
-
+  const [bugs, setBug] = useState([]);
+  
   useEffect(() => {
-    axios.get(`http://localhost:3000/api/v1/projects/${params.id}/bugs`)
-    .then(res => {
-        console.log(res)
-        setBug(res.data)
-    })
-    .catch(Error)
-}, [params.id])
-    return (
-        <>		
-<br />
-          <div className="card-title">
-              {bugs.empty ?
-              <p>Project Name: {bugs[0] && bugs[0].project_name}</p> :
-            <h4>Project Name: {bugs[0] && bugs[0].project_name}</h4>}
-            <li className="list-group-item"><i className="fa fa-bug fa-2x"></i>Bug Count: </li>
-          </div>
-          <table className="table">
-            <thead>
-              <tr>
-                <th scope="col">Bug id#</th>
-                <th scope="col">Bug Title</th>
-                <th scope="col">Bug Status</th>
-                <th scope="col">Pick/Not Picked</th>
-                <th scope="col"></th>
-              </tr>
-            </thead>
-            {bugs.map(bug =>
-            <tbody key={bug.id}>
-              <td>{bug.id}</td>
-              <td>{bug.title}</td>
-              <td>{bug.status}</td>
-              <td>{bug.assigned_to_id ? <i className='badge rounded-pill badge-success'>Picked</i> : <i className="badge rounded-pill badge-warning">Not Picked</i>}</td>
-            </tbody>)}
-          </table>
-        </>
-    )
+    BugList(params.id).then( response =>{
+      setBug(response)
+    }).catch((Error) => {
+      
+    })}, [params.id] )
+
+  return (
+    <>
+      <br />
+      <div className="card-title">
+        {bugs.empty ? (
+          <p>Project Name: {bugs[0] && bugs[0].project_name}</p>
+        ) : (
+          <h4>Project Name: {bugs[0] && bugs[0].project_name}</h4>
+        )}
+        <li className="list-group-item">
+          <i className="fa fa-bug fa-2x"></i>Bug Count:{" "}
+        </li>
+      </div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Bug id#</th>
+            <th scope="col">Bug Title</th>
+            <th scope="col">Bug Status</th>
+            <th scope="col">Pick/Not Picked</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        {bugs.map((bug) => (
+          <tbody key={bug.id}>
+            <td>{bug.id}</td>
+            <td>{bug.title}</td>
+            <td>{bug.status}</td>
+            <td>
+              {bug.assigned_to_id ? (
+                <i className="badge rounded-pill badge-success">Picked</i>
+              ) : (
+                <i className="badge rounded-pill badge-warning">Not Picked</i>
+              )}
+            </td>
+          </tbody>
+        ))}
+      </table>
+    </>
+  );
 }
